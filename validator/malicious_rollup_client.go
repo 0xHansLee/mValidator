abfeff6a562d2935728c39858afe6151d4755d1c
0xHansLee <hans@lightscale.io>
2023-06-26 10:19:48 +0900

    chore: restore mock call context

--- validator/malicious_rollup_client.go
+++ validator/malicious_rollup_client.go
@@ -2,11 +2,14 @@ package validator
 
 import (
 	"context"
+	"math/rand"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/kroma-network/kroma/components/node/client"
+	"github.com/kroma-network/kroma/components/node/eth"
+	"github.com/kroma-network/kroma/components/node/testutils"
 )
 
 type MaliciousRollupRPC struct {
@@ -38,16 +41,19 @@ func (r *MaliciousRollupRPC) CallContext(ctx context.Context, result interface{}
 	if method == "kroma_outputAtBlock" || method == "kroma_outputWithProofAtBlock" {
 		blockNumber := args[0].(hexutil.Uint64)
 
-		if r.targetBlockNumber != nil && blockNumber >= *r.targetBlockNumber {
-			err := r.rpc.CallContext(ctx, &result, method, blockNumber-1)
-			if err != nil {
-				return err
-			}
-			return nil
-		}
 		err := r.rpc.CallContext(ctx, &result, method, blockNumber)
 		if err != nil {
 			return err
+		}
+		if r.targetBlockNumber != nil && *r.targetBlockNumber <= blockNumber {
+			rng := rand.New(rand.NewSource(int64(blockNumber)))
+
+			s := result.(**eth.OutputResponse)
+			(*s).OutputRoot = eth.Bytes32(testutils.RandomHash(rng))
+			(*s).WithdrawalStorageRoot = testutils.RandomHash(rng)
+			(*s).StateRoot = testutils.RandomHash(rng)
+
+			return nil
 		}
 	}
 
